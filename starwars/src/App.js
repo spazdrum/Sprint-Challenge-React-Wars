b5a9ae44c8e8608399b7bd152fb94bedd632fece
spazdrum <spazdrum@gmail.com>
2020-04-17 22:58:34 -0400

    Completed app.js and Character.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import Characters from './components/Character';
 
 
 const App = () => {
@@ -10,13 +11,25 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [starData, setStarData] = useState([]);
+  useEffect(() => {
+    axios
+    .get("https://swapi.py4e.com/api/people/")
+    .then(res => setStarData(res.data.results))
+    .catch(err => console.log(err));
+  }, []);
   
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {starData.map((people, index) => {
+        return (
+          <Characters key={index} name={people.name} height={people.height}
+          mass={people.mass} hair={people.hair_color} skin={people.skin_color} />
+        )
+      })};
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,3 +1,19 @@
 // Write your Character component here
 import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+import axios from 'axios';
+
+function Characters (props) {
+    return (
+        <div>
+            <section>
+                <h3>Name: {props.name}</h3>
+                <h3>Height: {props.height}</h3>
+                <h3>Mass: {props.mass}</h3>
+                <h3>Hair Color: {props.hair}</h3>
+                <h3>Skin Color: {props.skin}</h3>
+            </section>
+        </div>
+    )
+}
+
+export default Characters;
