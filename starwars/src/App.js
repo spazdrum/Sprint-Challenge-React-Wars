3c0213fde5f3740326c6b9e3ad8c8a14d2bc8934
spazdrum <spazdrum@gmail.com>
2020-04-17 22:23:49 -0400

    Initial Commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Axios from 'axios';
-import Character from './components/Character';
+import axios from 'axios';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,6 +11,7 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
