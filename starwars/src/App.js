30a30966f88a81daca6890881700692f1cff3b94
spazdrum <spazdrum@gmail.com>
2020-04-17 21:25:44 -0400

    Initial Commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
