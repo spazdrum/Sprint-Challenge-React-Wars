d44aab9fbfe680314167eaeee556989d94ce6ada
spazdrum <spazdrum@gmail.com>
2020-04-17 23:46:43 -0400

    finished styling character.js

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,26 +2,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const section = styled.div`
+const AppDiv = styled.div`
+display: flex;
+justify-content: space-around;
+`
+
+const Appsection = styled.section`
 width: 30%;
-padding: 2%;
-background-color: red;
+padding-left: 10px;
+background-color: #ffffff33;
 border-radius: 8px;
 box-sizing: border-box;
 margin: 20px 0;
+text-align: left;
+`
+
+const AppH3Style = styled.h3`
+text-align: center;
+padding-bottom: 20px;
+padding-top: 10px;
 `
 
 function Characters (props) {
     return (
-        <div>
-            <section>
-                <h3>Name: {props.name}</h3>
+        <AppDiv>
+            <Appsection>
+                <AppH3Style>Name: {props.name}</AppH3Style>
                 <h3>Height: {props.height}</h3>
                 <h3>Mass: {props.mass}</h3>
                 <h3>Hair Color: {props.hair}</h3>
                 <h3>Skin Color: {props.skin}</h3>
-            </section>
-        </div>
+            </Appsection>
+        </AppDiv>
     );
 };
 
