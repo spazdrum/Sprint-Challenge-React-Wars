c83f5f08f79af0fc9b6cc2aad10a60ab6e6d1f22
spazdrum <spazdrum@gmail.com>
2020-04-17 23:06:51 -0400

    compiled data

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,6 +1,5 @@
 // Write your Character component here
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 function Characters (props) {
     return (
